f5c2974863d10cb62acfac9d32a0cc3a3cfdf881
Erik Osmond <erikosmond@gmail.com>
2015-02-20 10:29:48 -0500

    moved some links around

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 	def index
 		@cocktail = Cocktail.order("RANDOM()").first
 		@ingredient = Ingredient.order("RANDOM()").first
+		@ingredient_families = IngredientCategory.find(1).ingredient_families
 	end
 
 end
